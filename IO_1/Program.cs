b1b6c2cd88b84e5ef0ebeae28dc8915b9f14e479
Szamaragdowy <springi775@gmail.com>
2017-10-24 22:24:58 +0200

    5 done
    All 5

---
+++ IO_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ThreadProc, new object[] {500,1});
+            ThreadPool.QueueUserWorkItem(ThreadProc, new object[] {1000,2});
+
+            Thread.Sleep(3000);
+        }
+
+        static void ThreadProc(Object stateInfo)
+        {
+            var time = ((object[])stateInfo)[0];
+            var number = ((object[])stateInfo)[1];
+            Thread.Sleep((int)time);
+
+            Console.WriteLine("Hej jestem watek numer: " + number + " i czekalem " + time);
+        }
+    }
+}
