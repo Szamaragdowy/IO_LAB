ba2d4e56098cbda04fb8936755f33fc8635d123a
Szamaragdowy <springi775@gmail.com>
2017-10-30 17:02:57 +0100

    2 end

--- IO_2_8/Program.cs
+++ IO_2_8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,73 +12,106 @@ namespace IO_2_8
     {
         delegate int DelegateType(int arguments);
 
-        static DelegateType Silnia_i;
-        static DelegateType Silnia_r;
-        static DelegateType Fibonaci_i;
-        static DelegateType Fibonaci_r;
+        
 
-        static int silnia_r(int arguments)
+        static int silnia_r(int n)
         {
-            int i = arguments;
+            int factorial = 1;
 
-            if (i < 1)
-                return 1;
-            else
-                return i * silnia_i(i - 1);
+            if (n <= 1) return 1;
+
+            factorial = n * silnia_r(n - 1);
+
+            return factorial;
 
         }
 
-        static int silnia_i(int arguments)
+        static int silnia_i(int n)
         {
-            int n = arguments;
-           
-            int result = 1;
-            for (int i = 1; i < n; i++)
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
             {
-                result *= i;
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        static int fibonaci_r(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return fibonaci_r(n - 1) + fibonaci_r(n - 2);
+        }
+
+        static int fibonaci_i(int n)
+        {
+            int firstnumber = 0;
+            int secondnumber = 1;
+            int result = 0;
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
             }
             return result;
-
-        }
-
-        static int fibonaci_r(int arguments)
-        {
-            int n =  arguments;
-            if ((n == 1) || (n == 2))
-                return 1;
-            else
-                return fibonaci_r(n - 1) + fibonaci_r(n - 2);
-        }
-
-        static int fibonaci_i(int arguments)
-        {
-            return 0;
         }
 
         static void Main(string[] args)
         {
-            Silnia_i = new DelegateType(silnia_i);
-            Silnia_r = new DelegateType(silnia_r);
-            Fibonaci_i = new DelegateType(fibonaci_i);
-            Fibonaci_r= new DelegateType(fibonaci_r);
+            int a = 15;
+            int b = 40;
 
-            IAsyncResult ar_s_i = Silnia_i.BeginInvoke(5, null, null);
-            IAsyncResult ar_s_r = Silnia_r.BeginInvoke(5, null, null);
-            IAsyncResult ar_f_i = Fibonaci_i.BeginInvoke(20, null, null);
-            IAsyncResult ar_f_r = Fibonaci_r.BeginInvoke(20, null, null);
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
 
-         
-
-
+            DelegateType Silnia_i = new DelegateType(silnia_i);
+            IAsyncResult ar_s_i = Silnia_i.BeginInvoke(a, null, null);
             int wynik_silnia_i = Silnia_i.EndInvoke(ar_s_i);
-            int wynik_silnia_r = Silnia_r.EndInvoke(ar_s_r);
-            int wynik_fibonaci_i = Fibonaci_i.EndInvoke(ar_f_i);
-            int wynik_fibonaci_r = Fibonaci_r.EndInvoke(ar_f_r);
+            stopWatch1.Stop();
+            Console.WriteLine("Silnia Iteracyjnie {0} is {1}. Time elapsed: {2}ms",
+                a, wynik_silnia_i, stopWatch1.ElapsedMilliseconds);
 
-            Console.WriteLine("wynik silni iteracyjnie: " + wynik_silnia_i);
-            Console.WriteLine("wynik silni rekurencyjnie: " + wynik_silnia_r);
-            Console.WriteLine("wynik fibonaci iteracyjnie: " + wynik_fibonaci_i);
-            Console.WriteLine("wynik fibonaci rekurencyjnie: " + wynik_fibonaci_r);
+
+
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+
+            DelegateType Silnia_r = new DelegateType(silnia_r);  
+            IAsyncResult ar_s_r = Silnia_r.BeginInvoke(a, null, null);
+            int wynik_silnia_r = Silnia_r.EndInvoke(ar_s_r);
+            stopWatch2.Stop();
+            Console.WriteLine("Silnia rekurencyjnie {0} is {1}. Time elapsed: {2}ms", 
+                a, wynik_silnia_r, stopWatch2.ElapsedMilliseconds);
+
+
+            Stopwatch stopWatch3 = new Stopwatch();
+            stopWatch3.Start();
+
+
+            DelegateType Fibonaci_i = new DelegateType(fibonaci_i);
+            IAsyncResult ar_f_i = Fibonaci_i.BeginInvoke(b, null, null);
+            int wynik_fibonaci_i = Fibonaci_i.EndInvoke(ar_f_i);
+            stopWatch3.Stop();
+            Console.WriteLine("Fibonaci Iteracyjnie {0} is {1}. Time elapsed: {2}ms", 
+                b, wynik_fibonaci_i, stopWatch3.ElapsedMilliseconds);
+
+
+            Stopwatch stopWatch4 = new Stopwatch();
+            stopWatch4.Start();
+
+
+            DelegateType Fibonaci_r = new DelegateType(fibonaci_r);
+            IAsyncResult ar_f_r = Fibonaci_r.BeginInvoke(b, null, null);
+            int wynik_fibonaci_r = Fibonaci_r.EndInvoke(ar_f_r);
+            stopWatch4.Stop();
+            Console.WriteLine("fibonaci rekurencyjnie {0} is {1}. Time elapsed: {2}ms", 
+                b, wynik_fibonaci_r, stopWatch4.ElapsedMilliseconds);
+
         }
 
     }
