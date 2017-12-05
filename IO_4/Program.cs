2737e0864d9eae406bd86957f18655c731aed93b
Szamaragdowy <springi775@gmail.com>
2017-12-05 19:56:04 +0100

    fin

---
+++ IO_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IO_4
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Server server = new Server("127.0.0.1", 2048,1024);
+            var t = server.start();
+
+            //Task.WaitAll();
+
+            Client client = new Client("127.0.0.1", 2048);
+
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+
+
+            client.KeepPinging("test", ct);
+
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("zakonczenia dzialania 5 s");
+        }
+    }
+}
+
+
